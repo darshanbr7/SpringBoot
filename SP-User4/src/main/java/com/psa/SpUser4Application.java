25576319dfa631ae775bb45db3700faa3fc4cf5f
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-03-29 13:04:16 +0530

    spring

---
+++ SP-User4/src/main/java/com/psa/SpUser4Application.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpUser4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpUser4Application.class, args);
+	}
+
+}
---
+++ SP-User5/src/main/java/com/user/SpUser5Application.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpUser5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpUser5Application.class, args);
+	}
+
+}
